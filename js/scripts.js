57a9bcfaa1dc28590c0afb84ade8be9e882b66b1
Cory West-Forbes and Matt Miller <matt@>
2018-03-26 11:23:17 -0700

    add showList function and refactor code

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,26 @@
 $(function(){
   var groceryItems = [];
   var itemNumbers = [1, 2, 3, 4];
+
   $('#groceryList').submit(function(event){
-    itemNumbers.forEach(function(itemNumber){
-      var userItem = $('#item' + itemNumber).val();
-      groceryItems.push(userItem);
-      console.log(groceryItems);
-    });
+
+    function createShoppingList() {
+      itemNumbers.forEach(function(itemNumber){
+        var userItem = $('#item' + itemNumber).val();
+        groceryItems.push(userItem);
+        // console.log(groceryItems);
+      });
+    }
+
+    function showList() {
+      $('#groceryList').hide();
+      groceryItems.forEach(function(groceryItem){
+        $('ul').append('<li>' + groceryItem + '</li>');
+      });
+    }
+
+    createShoppingList();
+    showList();
     event.preventDefault();
   });
 });
